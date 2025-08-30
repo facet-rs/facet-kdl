f2fece6fc395da01f4ba484184d9af1d92323ecb
cactus <melody@boringcactus.com>
2025-08-30 16:57:46 -0600

    successfully deserialize the canon KDL example document

--- tests/basic.rs
+++ tests/basic.rs
@@ -8,7 +8,6 @@ fn it_works() {
 }
 
 #[test]
-#[ignore]
 fn basic_node() {
     // QUESTION: I don't know when this would be particularly good practice, but it could be nice if `facet` shipped
     // some sort of macro that allowed libraries to rename the Facet trait / attributes.unwrap() This might make it clearer
@@ -40,4 +39,159 @@ fn basic_node() {
     dbg!(Basic::SHAPE);
 
     let _basic: Basic = facet_kdl::from_str(kdl).unwrap();
+}
+
+#[test]
+fn canon_example() {
+    #[derive(Facet, PartialEq, Debug)]
+    struct Root {
+        #[facet(child)]
+        package: Package,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Package {
+        #[facet(child)]
+        name: Name,
+        #[facet(child)]
+        version: Version,
+        #[facet(child)]
+        dependencies: Dependencies,
+        #[facet(child)]
+        scripts: Scripts,
+        #[facet(child)]
+        #[facet(rename = "the-matrix")]
+        the_matrix: TheMatrix,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Name {
+        #[facet(argument)]
+        name: String,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Version {
+        #[facet(argument)]
+        version: String,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Dependencies {
+        #[facet(children)]
+        dependencies: Vec<Dependency>,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Scripts {
+        #[facet(children)]
+        scripts: Vec<Script>,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct TheMatrix {
+        #[facet(arguments)]
+        data: Vec<u8>,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Dependency {
+        #[facet(node_name)]
+        name: String,
+        #[facet(argument)]
+        version: String,
+        #[facet(property)]
+        optional: Option<bool>,
+        #[facet(property)]
+        alias: Option<String>,
+    }
+
+    #[derive(Facet, PartialEq, Debug)]
+    struct Script {
+        #[facet(node_name)]
+        name: String,
+        #[facet(argument)]
+        body: String,
+    }
+
+    let kdl = indoc! {r##"
+        package {
+            name my-pkg
+            version "1.2.3"
+
+            dependencies {
+                // Nodes can have standalone values as well as
+                // key/value pairs.
+                lodash "^3.2.1" optional=#true alias=underscore
+            }
+
+            scripts {
+                // "Raw" and dedented multi-line strings are supported.
+                message """
+                    hello
+                    world
+                    """
+                build #"""
+                    echo "foo"
+                    node -c "console.log('hello, world!');"
+                    echo "foo" > some-file.txt
+                    """#
+            }
+
+            // `\` breaks up a single node across multiple lines.
+            the-matrix 1 2 3 \
+                       4 5 6 \
+                       7 8 9
+
+            // "Slashdash" comments operate at the node level,
+            // with just `/-`.
+            /-this-is-commented {
+                this entire node {
+                    is gone
+                }
+            }
+        }
+    "##};
+
+    let root: Root = facet_kdl::from_str(kdl).unwrap();
+    assert_eq!(
+        root,
+        Root {
+            package: Package {
+                name: Name {
+                    name: "my-pkg".to_string()
+                },
+                version: Version {
+                    version: "1.2.3".to_string()
+                },
+                dependencies: Dependencies {
+                    dependencies: vec![Dependency {
+                        name: "lodash".to_string(),
+                        version: "^3.2.1".to_string(),
+                        optional: Some(true),
+                        alias: Some("underscore".to_string())
+                    }]
+                },
+                scripts: Scripts {
+                    scripts: vec![
+                        Script {
+                            name: "message".to_string(),
+                            body: "hello\nworld".to_string()
+                        },
+                        Script {
+                            name: "build".to_string(),
+                            body: indoc! {r#"
+                                echo "foo"
+                                node -c "console.log('hello, world!');"
+                                echo "foo" > some-file.txt"#}
+                            .to_string()
+                        }
+                    ]
+                },
+                the_matrix: TheMatrix {
+                    data: vec![1, 2, 3, 4, 5, 6, 7, 8, 9]
+                },
+            }
+        }
+    );
 }
